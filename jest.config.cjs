ac37e43e26c19a1633a6002fc33da2869b9dd3e4
Aymir Sunchugashev <as4n.tsu@gmail.com>
2026-03-02 15:38:38 +0700

    переименовал директории с компонентами && установил зависимости для тестирования

---
+++ jest.config.cjs
@@ -0,0 +1,25 @@
+const config = {
+  testEnvironment: "jsdom",
+
+  transform: {
+    "^.+\\.tsx?$": [
+      "ts-jest",
+      {
+        tsconfig: "tsconfig.app.json",
+      },
+    ],
+  },
+
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+
+  testPathIgnorePatterns: ["/node_modules/", "/dist/"],
+
+  collectCoverageFrom: [
+    "src/**/*.{ts,tsx}",
+    "!src/**/*.d.ts",
+    "!src/**/*.test.{ts,tsx}",
+    "!src/**/index.{ts,tsx}",
+  ],
+};
+
+module.exports = config;
