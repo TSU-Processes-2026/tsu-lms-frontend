bfccd5a8298487a14f046d94bb6eeac8f7e3d1d1
Aymir Sunchugashev <as4n.tsu@gmail.com>
2026-03-03 17:35:06 +0700

    Переписал тесты для хука useLoginForm && Написал интеграционные тесты для проверки полного сценария авторизации пользователя

--- src/pages/Login/hooks/useLoginForm.test.ts
+++ src/pages/Login/hooks/useLoginForm.test.ts
@@ -1,11 +1,34 @@
 import { act, renderHook } from "@testing-library/react";
 import { useLoginForm } from "./useLoginForm";
+import { AxiosResponse } from "axios";
+import { TokenResponse } from "../../../types/token/TokenResponse";
+import { login } from "../../../api/authorization/login";
 
-describe("useLoginForm validation data tests", () => {
-  const mockOnSubmit = jest.fn();
+jest.mock("../../../api/authorization/login");
+const mockedLogin = login as jest.MockedFunction<typeof login>;
+
+const mockLocalStorage = {
+  setItem: jest.fn(),
+  getItem: jest.fn(),
+  removeItem: jest.fn(),
+  clear: jest.fn(),
+};
+
+const mockEvent = {
+  preventDefault: jest.fn(),
+} as unknown as React.FormEvent;
+
+Object.defineProperty(window, "localStorage", {
+  value: mockLocalStorage,
+});
+
+describe("useLoginForm: Тесты валидации входных данных", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
 
   test("Поля формы должны инициализироваться пустыми значениями", () => {
-    const { result } = renderHook(() => useLoginForm);
+    const { result } = renderHook(() => useLoginForm());
 
     expect(result.current.username).toBe("");
     expect(result.current.password).toBe("");
@@ -33,7 +56,7 @@ describe("useLoginForm validation data tests", () => {
     expect(result.current.password).toBe("password");
   });
 
-  test("При валидации логина длиной менее 3 символов возвращается ошибка", () => {
+  test("При валидации логина длиной менее 3 символов возвращается ошибка", async () => {
     const { result } = renderHook(() => useLoginForm());
     const errorMessage = "Длина логина должна быть не менее 3";
 
@@ -41,30 +64,28 @@ describe("useLoginForm validation data tests", () => {
       result.current.setUsername("ло");
     });
 
-    act(() => {
-      result.current.validateForm();
-      result.current.setErrorMessage(errorMessage);
+    await act(async () => {
+      await result.current.onSubmit(mockEvent);
     });
 
     expect(result.current.errorMessage).toBe(errorMessage);
   });
 
-  test("При валидации логина длиной в 3 символа сообщение об ошибке отсутствует", () => {
+  test("При валидации логина длиной в 3 символа сообщение об ошибке отсутствует", async () => {
     const { result } = renderHook(() => useLoginForm());
 
     act(() => {
       result.current.setUsername("лог");
     });
 
-    act(() => {
-      result.current.validateForm();
-      result.current.setErrorMessage("");
+    await act(async () => {
+      await result.current.onSubmit(mockEvent);
     });
 
     expect(result.current.errorMessage).toBe("");
   });
 
-  test("При валидации пароля длиной менее 6 символов возвращается ошибка", () => {
+  test("При валидации пароля длиной менее 6 символов возвращается ошибка", async () => {
     const { result } = renderHook(() => useLoginForm());
     const errorMessage = "Длина пароля должна быть от 6 до 20 символов";
 
@@ -72,15 +93,14 @@ describe("useLoginForm validation data tests", () => {
       result.current.setPassword("passw");
     });
 
-    act(() => {
-      result.current.validateForm();
-      result.current.setErrorMessage(errorMessage);
+    await act(async () => {
+      await result.current.onSubmit(mockEvent);
     });
 
     expect(result.current.errorMessage).toBe(errorMessage);
   });
 
-  test("При валидации пароля длиной более 20 символов возвращается ошибка", () => {
+  test("При валидации пароля длиной более 20 символов возвращается ошибка", async () => {
     const { result } = renderHook(() => useLoginForm());
     const errorMessage = "Длина пароля должна быть от 6 до 20 символов";
 
@@ -88,45 +108,42 @@ describe("useLoginForm validation data tests", () => {
       result.current.setPassword("passwordpasswordpassword");
     });
 
-    act(() => {
-      result.current.validateForm();
-      result.current.setErrorMessage(errorMessage);
+    await act(async () => {
+      await result.current.onSubmit(mockEvent);
     });
 
     expect(result.current.errorMessage).toBe(errorMessage);
   });
 
-  test("При валидации пароля длиной 6 символов сообщение об ошибке отсутствует", () => {
+  test("При валидации пароля длиной 6 символов сообщение об ошибке отсутствует", async () => {
     const { result } = renderHook(() => useLoginForm());
 
     act(() => {
       result.current.setPassword("passwo");
     });
 
-    act(() => {
-      result.current.validateForm();
-      result.current.setErrorMessage("");
+    await act(async () => {
+      await result.current.onSubmit(mockEvent);
     });
 
     expect(result.current.errorMessage).toBe("");
   });
 
-  test("При валидации пароля длиной 20 символов сообщение об ошибке отсутствует", () => {
+  test("При валидации пароля длиной 20 символов сообщение об ошибке отсутствует", async () => {
     const { result } = renderHook(() => useLoginForm());
 
     act(() => {
       result.current.setPassword("passwordpassword1234");
     });
 
-    act(() => {
-      result.current.validateForm();
-      result.current.setErrorMessage("");
+    await act(async () => {
+      await result.current.onSubmit(mockEvent);
     });
 
     expect(result.current.errorMessage).toBe("");
   });
 
-  test("При валидации формы с валидными данными сообщение об ошибке отсутствует", () => {
+  test("При валидации формы с валидными данными сообщение об ошибке отсутствует", async () => {
     const { result } = renderHook(() => useLoginForm());
 
     act(() => {
@@ -134,17 +151,154 @@ describe("useLoginForm validation data tests", () => {
       result.current.setPassword("password123");
     });
 
-    act(() => {
-      result.current.validateForm();
-      result.current.setErrorMessage("");
+    await act(async () => {
+      await result.current.onSubmit(mockEvent);
     });
 
     expect(result.current.errorMessage).toBe("");
   });
 });
 
-describe("navigation tests", () => {
-  test("После успешной авторизации ползователь должен быть перенаправлен на главную страницу", () => {});
+describe("useLoginForm: Тесты для проверки полного сценария авторизации пользователя", () => {
+  const mockTokenResponse: TokenResponse = {
+    tokenType: "Bearer",
+    accessToken: "mock-access-token",
+    refreshToken: "mock-refresh-token",
+    expiresIn: 900,
+    refreshExpiresIn: 60480,
+    userId: "123",
+    sessionId: "mock-session",
+  };
 
-  test('При нажатии на кнопку " Зарегистрироваться" пользователь должен быть перенаправлен на страницу регистрации', () => {});
+  const mockAxiosResponse: Partial<AxiosResponse<TokenResponse>> = {
+    data: mockTokenResponse,
+    status: 200,
+    statusText: "OK",
+    headers: {},
+    config: {} as any,
+  };
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  test("Успешный сценарий: после успешного запроса на сервер, данные сохраняются в localStorage", async () => {
+    mockedLogin.mockResolvedValueOnce(
+      mockAxiosResponse as AxiosResponse<TokenResponse>,
+    );
+    const { result } = renderHook(() => useLoginForm());
+
+    act(() => {
+      result.current.setUsername("testuser");
+      result.current.setPassword("password123");
+    });
+
+    await act(async () => {
+      await result.current.onSubmit(mockEvent);
+    });
+
+    expect(mockedLogin).toHaveBeenCalledWith({
+      username: "testuser",
+      password: "password123",
+    });
+    expect(mockLocalStorage.setItem).toHaveBeenCalledWith(
+      "accessToken",
+      mockTokenResponse.accessToken,
+    );
+    expect(mockLocalStorage.setItem).toHaveBeenCalledWith(
+      "refreshToken",
+      mockTokenResponse.refreshToken,
+    );
+    expect(mockLocalStorage.setItem).toHaveBeenCalledWith(
+      "userId",
+      mockTokenResponse.userId,
+    );
+    expect(mockLocalStorage.setItem).toHaveBeenCalledWith(
+      "sessionId",
+      mockTokenResponse.sessionId,
+    );
+    expect(result.current.errorMessage).toBe("");
+  });
+
+  test("Сценарий с 400 ошибкой", async () => {
+    const errorResponse = {
+      response: {
+        status: 400,
+        data: { message: "Неверные данные" },
+      },
+      isAxiosError: true,
+    };
+
+    mockedLogin.mockRejectedValueOnce(errorResponse);
+
+    const { result } = renderHook(() => useLoginForm());
+
+    act(() => {
+      result.current.setUsername("va");
+      result.current.setPassword("pass123");
+    });
+
+    await act(async () => {
+      await result.current.onSubmit(mockEvent);
+    });
+
+    expect(mockedLogin).toHaveBeenCalledWith({
+      username: "va",
+      password: "pass123",
+    });
+
+    expect(mockLocalStorage.setItem).not.toHaveBeenCalled();
+    expect(result.current.errorMessage).not.toBe("");
+    expect(result.current.errorMessage).toBe("Неверные данные");
+  });
+
+  test("Сценарий с 401 ошибкой", async () => {
+    mockedLogin.mockRejectedValueOnce({
+      response: {
+        status: 401,
+        data: { message: "Неверный пароль" },
+      },
+      isAxiosError: true,
+    });
+
+    const { result } = renderHook(() => useLoginForm());
+
+    act(() => {
+      result.current.setUsername("validuser");
+      result.current.setPassword("validpass123");
+    });
+
+    await act(async () => {
+      await result.current.onSubmit(mockEvent);
+    });
+
+    expect(mockedLogin).toHaveBeenCalled();
+    expect(mockLocalStorage.setItem).not.toHaveBeenCalled();
+    expect(result.current.errorMessage).toBe("Неверный пароль");
+  });
+
+  test("Сценарий с 500 ошибкой", async () => {
+    mockedLogin.mockRejectedValueOnce({
+      response: {
+        status: 500,
+        data: { message: "Ошибка сервера" },
+      },
+      isAxiosError: true,
+    });
+
+    const { result } = renderHook(() => useLoginForm());
+
+    act(() => {
+      result.current.setUsername("validuser");
+      result.current.setPassword("validpass123");
+    });
+
+    await act(async () => {
+      await result.current.onSubmit(mockEvent);
+    });
+
+    expect(mockedLogin).toHaveBeenCalled();
+    expect(mockLocalStorage.setItem).not.toHaveBeenCalled();
+    expect(result.current.errorMessage).toBe("Ошибка сервера");
+  });
 });
--- src/pages/Login/hooks/useLoginForm.ts
+++ src/pages/Login/hooks/useLoginForm.ts
@@ -1,1 +1,23 @@
-export const useLoginForm = () => {};
+import { useState } from "react";
+
+export const useLoginForm = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const validateForm = () => {};
+  const onSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+  };
+
+  return {
+    username,
+    password,
+    errorMessage,
+    setUsername,
+    setPassword,
+    setErrorMessage,
+    validateForm,
+    onSubmit,
+  };
+};
