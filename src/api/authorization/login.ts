b2c114ce6b6cfa8d28a08e87b1ea3df2a7b03a0f
Aymir Sunchugashev <as4n.tsu@gmail.com>
2026-03-03 16:44:46 +0700

    Реализовал метод login()

--- src/api/authorization/login.ts
+++ src/api/authorization/login.ts
@@ -1,9 +1,25 @@
-import type { AxiosResponse } from "axios";
+import axios, { type AxiosResponse } from "axios";
 import type { LoginRequest } from "../../types/auth/LoginRequest";
 import type { TokenResponse } from "../../types/token/TokenResponse";
+import { DEV_URL, PROD_URL, MOCK_URL } from "../../constants/config/config";
+
+const BASE_URL = DEV_URL || PROD_URL || MOCK_URL;
 
 export const login = async (
   data: LoginRequest,
-): Promise<AxiosResponse<TokenResponse, any, {}> | null> => {
-  return null;
+): Promise<AxiosResponse<TokenResponse>> => {
+  try {
+    const response = await axios.post<TokenResponse>(
+      `${BASE_URL}/auth/login`,
+      data,
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      },
+    );
+    return response;
+  } catch (error) {
+    throw error;
+  }
 };
--- src/constants/config/config.ts
+++ src/constants/config/config.ts
@@ -1,3 +1,3 @@
-export const MOCK_URL = "http://localhost:3001";
+export const MOCK_URL = "http://localhost:3001/api";
 export const DEV_URL = undefined;
 export const PROD_URL = undefined;
