bdc111752b969ba337962bfc0509952d23f87313
Aymir Sunchugashev <as4n.tsu@gmail.com>
2026-03-04 15:23:57 +0700

    Написал реализацию валидации формы

--- src/api/register/register.ts
+++ src/api/register/register.ts
@@ -1,7 +1,25 @@
-import { type AxiosResponse } from "axios";
+import axios, { type AxiosResponse } from "axios";
 import { RegisterRequest } from "../../types/auth/RegisterRequest";
 import { UserResponse } from "../../types/user/UserResponse";
+import { DEV_URL, PROD_URL, MOCK_URL } from "../../constants/config/config";
+
+const BASE_URL = DEV_URL || PROD_URL || MOCK_URL;
 
 export const register = async (
   data: RegisterRequest,
-): Promise<AxiosResponse<UserResponse>> => {};
+): Promise<AxiosResponse<UserResponse>> => {
+  try {
+    const response = await axios.post<UserResponse>(
+      `${BASE_URL}/auth/register`,
+      data,
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      },
+    );
+    return response;
+  } catch (error) {
+    throw error;
+  }
+};
--- src/pages/Register/hooks/useRegisterForm.ts
+++ src/pages/Register/hooks/useRegisterForm.ts
@@ -1,4 +1,12 @@
 import { useState } from "react";
+import {
+  CONFIRM_PASSWORD_EMPTY_ERROR_MESSAGE,
+  CONFIRM_PASSWORD_FAILED_ERROR_MESSAGE,
+  LOGIN_EMPTY_ERROR_MESSAGE,
+  LOGIN_LENGTH_ERROR_MESSAGE,
+  PASSWORD_EMPTY_ERROR_MESSAGE,
+  PASSWORD_LENGTH_ERROR_MESSAGE,
+} from "../../../constants/error/errorMessages";
 
 export const useRegisterForm = () => {
   const [username, setUsername] = useState<string>("");
@@ -6,9 +14,56 @@ export const useRegisterForm = () => {
   const [confirmPassword, setConfirmPassword] = useState<string>("");
   const [errorMessage, setErrorMessage] = useState<string>("");
 
-  const onSubmit = async (e: React.FormEvent): Promise<void> => {};
+  const onSubmit = async (e: React.FormEvent): Promise<void> => {
+    e.preventDefault();
+    setErrorMessage("");
+    validateForm();
+    if (errorMessage !== "") {
+      return;
+    }
+  };
 
-  const validateForm = (): void => {};
+  const validateForm = (): void => {
+    if (!validateLogin()) return;
+    if (!validatePassword()) return;
+    if (!validateConfirmPassword()) return;
+  };
+
+  const validateLogin = () => {
+    if (username === null || username === undefined || username.trim() === "") {
+      setErrorMessage(LOGIN_EMPTY_ERROR_MESSAGE);
+      return false;
+    }
+    if (username.trim().length < 3 || username.trim().length > 50) {
+      setErrorMessage(LOGIN_LENGTH_ERROR_MESSAGE);
+      return false;
+    }
+    return true;
+  };
+
+  const validatePassword = () => {
+    if (password === null || password === undefined || password.trim() === "") {
+      setErrorMessage(PASSWORD_EMPTY_ERROR_MESSAGE);
+      return false;
+    }
+    if (password.trim().length < 6 || password.trim().length > 20) {
+      setErrorMessage(PASSWORD_LENGTH_ERROR_MESSAGE);
+      return false;
+    }
+    return true;
+  };
+
+  const validateConfirmPassword = (): boolean => {
+    if (password === null || password === undefined || password.trim() === "") {
+      setErrorMessage(CONFIRM_PASSWORD_EMPTY_ERROR_MESSAGE);
+      return false;
+    }
+    if (password.trim() !== confirmPassword.trim()) {
+      setErrorMessage(CONFIRM_PASSWORD_FAILED_ERROR_MESSAGE);
+      return false;
+    }
+    return true;
+  };
 
   return {
     username,
