38410b479d970a11c78b0c49d79ad583f4923d1a
Aymir Sunchugashev <as4n.tsu@gmail.com>
2026-03-05 16:04:20 +0700

    Реализовал логику запроса на обновление пары токенов

--- src/api/authorization/refresh.ts
+++ src/api/authorization/refresh.ts
@@ -1,7 +1,25 @@
-import { AxiosResponse } from "axios";
+import axios, { AxiosResponse } from "axios";
 import { RefreshTokenRequest } from "../../types/token/RefreshTokenRequest";
 import { TokenResponse } from "../../types/token/TokenResponse";
+import { DEV_URL, PROD_URL, MOCK_URL } from "../../constants/config/config";
+
+const BASE_URL = DEV_URL || PROD_URL || MOCK_URL;
 
 export const refresh = async (
   refreshToken: RefreshTokenRequest,
-): Promise<AxiosResponse<TokenResponse>> => {};
+): Promise<AxiosResponse<TokenResponse>> => {
+  try {
+    const response = await axios.post<TokenResponse>(
+      `${BASE_URL}/auth/refresh`,
+      refreshToken,
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      },
+    );
+    return response;
+  } catch (error) {
+    throw error;
+  }
+};
