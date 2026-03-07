7b2d7f9bfd62fbd31908cac1bb2a592af2e13157
Aymir Sunchugashev <as4n.tsu@gmail.com>
2026-03-07 20:55:45 +0700

    Реализовал валидацию

--- src/hooks/subject/useCreateSubject.ts
+++ src/hooks/subject/useCreateSubject.ts
@@ -1,14 +1,42 @@
+import { SUBJECT_DESCRIPTION_MAX_LENGTH_ERROR, SUBJECT_TITLE_EMPTY, SUBJECT_TITLE_ERROR } from "@/constants/error/errorMessages";
 import { FormEvent, useState } from "react"
 
 export const useCreateSubject = () => {
-    const [title, setTitle] = useState<string>("");
-    const [description, setDescription] = useState<string>("")
-    const [errorMessage, setErrorMessage] = useState<string>("")
-    const [isError, setIsError] = useState<boolean>(false)
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("")
+    const [errorMessage, setErrorMessage] = useState("")
 
-    const handleSubmit = async(e: FormEvent) => {}
+    const validateTitle = (): boolean => {
+        if(title.trim() === "") {
+            setErrorMessage(SUBJECT_TITLE_EMPTY)
+            return false
+        }
 
-    const isFormValid = (): boolean => { return false}
+        if(title.trim().length < 3 || title.trim().length > 50) {
+            setErrorMessage(SUBJECT_TITLE_ERROR)
+            return false
+        }
+         return true
+    } 
 
-    return {title, description, isError, errorMessage, setErrorMessage, setTitle, setDescription, handleSubmit, isFormValid}
+    const validateDescription = (): boolean => {
+        if(description.trim() !== "" && description.trim().length > 2000) {
+            setErrorMessage(SUBJECT_DESCRIPTION_MAX_LENGTH_ERROR)
+            return false
+        }
+        return true
+    }
+
+    const isFormValid = (): boolean => {
+        return validateTitle() && validateDescription()
+    };
+
+    const handleSubmit = async(e: FormEvent) => {
+        e.preventDefault();
+        setErrorMessage("")
+        if (!isFormValid()) return
+    }
+
+
+    return {title, description, errorMessage, setErrorMessage, setTitle, setDescription, handleSubmit, isFormValid}
 }
