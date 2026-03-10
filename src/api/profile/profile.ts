edec3d251704820345eacc2ea2f042d51eb8226f
Aymir Sunchugashev <as4n.tsu@gmail.com>
2026-03-10 15:37:56 +0700

    Реализовал логику запросов на сервер

--- src/api/profile/profile.ts
+++ src/api/profile/profile.ts
@@ -1,4 +1,21 @@
+import { ACCESS_TOKEN } from '@/constants/auth/auth';
+import { DEV_URL, MOCK_URL, PROD_URL } from '@/constants/config/config';
 import { UserResponse } from '@/types/user/UserResponse';
-import { AxiosResponse } from 'axios';
+import axios, { AxiosResponse } from 'axios';
 
-export const getProfile = (): Promise<AxiosResponse<UserResponse>> => {};
+const BASE_URL = DEV_URL || PROD_URL || MOCK_URL;
+
+export const getProfile = async (): Promise<AxiosResponse<UserResponse>> => {
+    const accessToken: string | null = localStorage.getItem(ACCESS_TOKEN);
+
+    try {
+        const response: AxiosResponse<UserResponse> = await axios.get(`${BASE_URL}/users/me`, {
+            headers: {
+                Authorization: `Bearer ${accessToken}`,
+            },
+        });
+        return response;
+    } catch (error) {
+        throw error;
+    }
+};
